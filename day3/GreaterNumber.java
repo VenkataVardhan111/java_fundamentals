a8abb21e15c17d1df85a190746e5922c12915869
Venkata Vardhan <venkatavardhan111@gmail.com>
2026-08-03 12:33:25 +0530

    Add files via upload

---
+++ day3/GreaterNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class GreaterNumber {
+ public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+ int a = sc.nextInt();
+ int b = sc.nextInt();
+ if (a > b)
+ System.out.println("Greater number is: " + a);
+ else
+ System.out.println("Greater number is: " + b);
+ }
+}
